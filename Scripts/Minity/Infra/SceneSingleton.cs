f67346f18f9fabff65ebaaf6fd66b19a12d92ca4
Buger404 <ris_vb@126.com>
2025-01-30 19:16:28 +0800

    feat: scene instance manager

---
+++ Scripts/Minity/Infra/SceneSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Minity.Infra
+{
+    public class SceneSingleton : MonoBehaviour
+    {
+        private static readonly Dictionary<Type, Object> instances = new();
+
+        public static T Get<T>() where T : Object
+        {
+            if (!instances.TryGetValue(typeof(T), out var instance))
+            {
+                throw new Exception($"'{typeof(T).Name}' instance is not registered.");
+            }
+
+            if (!instance)
+            {
+                throw new Exception($"'{typeof(T).Name}' instance is destroyed.");
+            }
+
+            return (T)instance;
+        }
+
+        public static void Register<T>(T instance) where T : Object
+        {
+            var type = typeof(T);
+            if (!instances.TryAdd(type, instance))
+            {
+                if (instances[type])
+                {
+                    Debug.LogWarning($"'{type.Name}' instance is duplicated.");
+                }
+                instances[type] = instance;
+            }
+        }
+    }
+}
