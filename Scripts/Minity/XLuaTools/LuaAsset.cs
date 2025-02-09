1abf083254146c2341b3f1da05cf0183d8c9a5f8
Buger404 <ris_vb@126.com>
2025-02-09 23:07:50 +0800

    feat: xLua tools
    define symbol MINITY_ENABLE_XLUA_TOOLS to enable it

---
+++ Scripts/Minity/XLuaTools/LuaAsset.cs
@@ -0,0 +1,31 @@
+﻿#if MINITY_ENABLE_XLUA_TOOLS
+using System;
+using UnityEngine;
+
+namespace Minity.XLuaTools
+{
+    public class LuaAsset : ScriptableObject
+    {
+        [SerializeField, HideInInspector]
+        private string _code;
+        
+        internal bool Validated = true;
+        
+        public string Code => _code;
+        
+        public override string ToString() => _code;
+
+        private void OnValidate()
+        {
+            Validated = true;
+        }
+
+        public static LuaAsset Create(string code)
+        {
+            var asset = CreateInstance<LuaAsset>();
+            asset._code = code;
+            return asset;
+        }
+    }
+}
+#endif
