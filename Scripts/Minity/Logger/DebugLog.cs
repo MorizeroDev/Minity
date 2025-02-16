a9732c4a6411508e347980da7f010ec39ba29e1f
Buger404 <ris_vb@126.com>
2025-02-16 16:05:42 +0800

    chore: debug logging

--- Scripts/Minity/Logger/DebugLog.cs
+++ Scripts/Minity/Logger/DebugLog.cs
@@ -8,6 +8,16 @@ namespace Minity.Logger
     internal static class DebugLog
     {
 #if UNITY_EDITOR
+        private const string PREFIX =
+            "<b><color=white>[</color>" +
+            "<color=#f39dff>M</color>" +
+            "<color=#eeb6ff>i</color>" +
+            "<color=#d7d2ff>n</color>" +
+            "<color=#c3dbff>i</color>" +
+            "<color=#b2dfff>t</color>" +
+            "<color=#a2e0ff>y</color>" +
+            "<color=white>]</color></b> ";
+        
         private enum LogLevel
         {
             Info, Warning, Error
@@ -23,19 +33,19 @@ namespace Minity.Logger
             _logLevel = (LogLevel)EditorPrefs.GetInt(LogLevelKey, (int)LogLevel.Warning);
         }
         
-        [MenuItem("Milutools/Log Level/Error", false, 2)]
+        [MenuItem("Minity/Log Level/Error", false, 2)]
         private static void SwitchLogLevelError()
         {
             SwitchLogLevel(LogLevel.Error);
         }
         
-        [MenuItem("Milutools/Log Level/Warning", false, 1)]
+        [MenuItem("Minity/Log Level/Warning", false, 1)]
         private static void SwitchLogLevelWarning()
         {
             SwitchLogLevel(LogLevel.Warning);
         }
         
-        [MenuItem("Milutools/Log Level/Info", false, 0)]
+        [MenuItem("Minity/Log Level/Info", false, 0)]
         private static void SwitchLogLevelInfo()
         {
             SwitchLogLevel(LogLevel.Info);
@@ -45,7 +55,7 @@ namespace Minity.Logger
         {
             _logLevel = level;
             EditorPrefs.SetInt(LogLevelKey, (int)level);
-            EditorUtility.DisplayDialog("Milutools", "Switched Milutools log level to " + level, "OK");
+            EditorUtility.DisplayDialog("Minity", "Switched Milutools log level to " + level, "OK");
         }
         
         internal static void Log(string content)
@@ -54,7 +64,7 @@ namespace Minity.Logger
             {
                 return;
             }
-            Debug.Log(content);
+            Debug.Log(PREFIX + content);
         }
         
         internal static void LogWarning(string content)
@@ -63,7 +73,7 @@ namespace Minity.Logger
             {
                 return;
             }
-            Debug.LogWarning(content);
+            Debug.LogWarning(PREFIX + content);
         }
         
         internal static void LogError(string content)
@@ -72,7 +82,7 @@ namespace Minity.Logger
             {
                 return;
             }
-            Debug.LogError(content);
+            Debug.LogError(PREFIX + content);
         }
 #else
         internal static void Log(string content)
