7df5d71f0b5685d753e950d527f19209d36edeb8
Buger404 <ris_vb@126.com>
2026-02-03 15:59:58 +0800

    feat: ui operation lock

---
+++ Scripts/Minity/UI/OperationLock/UIOperationLock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Minity.UI.OperationLock
+{
+    public sealed class UIOperationLock
+    {
+        private readonly SemaphoreSlim _sem = new(1, 1);
+        
+        public IDisposable? TryEnter()
+        {
+            if (!_sem.Wait(0))
+            {
+                return null;
+            }
+
+            return new Releaser(_sem);
+        }
+        
+        public async ValueTask<IAsyncDisposable> TryEnterAsync()
+        {
+            if (!await _sem.WaitAsync(0))
+            {
+                return null;
+            }
+
+            return new Releaser(_sem);
+        }
+
+        private readonly struct Releaser : IDisposable, IAsyncDisposable
+        {
+            private readonly SemaphoreSlim? _sem;
+
+            internal Releaser(SemaphoreSlim sem)
+            {
+                _sem = sem;
+            }
+
+            public void Dispose()
+            {
+                _sem?.Release();
+            }
+
+            public ValueTask DisposeAsync()
+            {
+                _sem?.Release();
+                return default;
+            }
+        }
+    }
+}
